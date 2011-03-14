d88da5e2e4d5c5c114e790a20250c555add197fb
johanneshagg <johanneshagg@4bcc0c5e-7632-61cd-5c51-6875b0865e43>
2011-03-14 15:07:49 +0000

    Version 1.1 release

---
+++ tags/v1-1/Jzon.cpp
@@ -0,0 +1,838 @@
+/*
+Copyright (c) 2011 Johannes Häggqvist
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#include "Jzon.h"
+
+#include <sstream>
+#include <fstream>
+
+namespace Jzon
+{
+	class FormatInterpreter
+	{
+	public:
+		FormatInterpreter(const Format &format) : format(format)
+		{
+			if (format.useTabs)
+			{
+				indentationChar = '\t';
+			}
+			else
+			{
+				for (unsigned int i = 0; i < format.indentSize; ++i)
+				{
+					indentationChar += ' ';
+				}
+			}
+
+			spacing = (format.spacing ? " " : "");
+			newline = (format.newline ? "\n" : spacing);
+		}
+
+		std::string GetIndentation(unsigned int level) const
+		{
+			if (!format.newline)
+				return "";
+			else
+			{
+				std::string ind;
+				for (unsigned int i = 0; i < level; ++i)
+				{
+					ind += indentationChar;
+				}
+				return ind;
+			}
+		}
+		
+		inline std::string GetNewline() const
+		{
+			return newline;
+		}
+		inline std::string GetSpacing() const
+		{
+			return spacing;
+		}
+
+	private:
+		const Format &format;
+		std::string indentationChar;
+		std::string newline;
+		std::string spacing;
+	};
+
+	Node::Node()
+	{
+	}
+	Node::~Node()
+	{
+	}
+
+	Object &Node::AsObject()
+	{
+		if (IsObject())
+			return static_cast<Object&>(*this);
+		else
+			throw TypeException();
+	}
+	Array &Node::AsArray()
+	{
+		if (IsArray())
+			return static_cast<Array&>(*this);
+		else
+			throw TypeException();
+	}
+	Value &Node::AsValue()
+	{
+		if (IsValue())
+			return static_cast<Value&>(*this);
+		else
+			throw TypeException();
+	}
+
+	NodePtr Node::Read(const std::string &json)
+	{
+		if (json.size() == 0)
+			return NodePtr(new Value());
+
+		std::string node;
+		Type lookFor;
+		int numOpen = 0;
+
+		switch (json.at(0))
+		{
+		case '{' : lookFor = T_OBJECT; break;
+		case '[' : lookFor = T_ARRAY; break;
+		default : lookFor = T_VALUE; break;
+		}
+
+		if (lookFor == T_VALUE)
+			return Value::Read(json);
+
+		for (std::string::const_iterator it = json.cbegin(); it != json.cend(); ++it)
+		{
+			char c = (*it);
+
+			node += c;
+
+			char openToken, closeToken;
+			if (lookFor == T_OBJECT)
+			{
+				openToken = '{';
+				closeToken = '}';
+			}
+			else if (lookFor == T_ARRAY)
+			{
+				openToken = '[';
+				closeToken = ']';
+			}
+
+			if (c == openToken)
+			{
+				++numOpen;
+			}
+			else if (c == closeToken)
+			{
+				--numOpen;
+
+				if (numOpen == 0)
+				{
+					if (lookFor == T_OBJECT)
+						return Object::Read(node);
+					else if (lookFor == T_ARRAY)
+						return Array::Read(node);
+				}
+			}
+		}
+
+		return NodePtr(new Value());
+	}
+
+
+	Value::Value()
+		: valueStr(""), type(VT_NULL)
+	{
+	}
+	Value::Value(const Value &rhs)
+	{
+		valueStr = rhs.valueStr;
+		type = rhs.type;
+	}
+	Value::Value(const std::string &value)
+		: valueStr(value), type(VT_STRING)
+	{
+	}
+	Value::Value(const char *value)
+		: valueStr(std::string(value)), type(VT_STRING)
+	{
+
+	}
+	Value::Value(const int value)
+		: type(VT_INT)
+	{
+		std::stringstream sstr;
+		sstr << value;
+		valueStr = sstr.str();
+	}
+	Value::Value(const double value)
+		: type(VT_DOUBLE)
+	{
+		std::stringstream sstr;
+		sstr << value;
+		valueStr = sstr.str();
+	}
+	Value::Value(const bool value)
+		: type(VT_BOOL)
+	{
+		if (value)
+			valueStr = "true";
+		else
+			valueStr = "false";
+	}
+	Value::~Value()
+	{
+	}
+
+	Node::Type Value::GetType() const
+	{
+		return T_VALUE;
+	}
+	Value::ValueType Value::GetValueType() const
+	{
+		return type;
+	}
+
+	bool Value::IsNull() const
+	{
+		return (type == VT_NULL);
+	}
+	std::string Value::AsString() const
+	{
+		if (IsNull())
+			return "null";
+		else
+			return valueStr;
+	}
+	int Value::AsInt() const
+	{
+		if (IsNull())
+			return 0;
+		else
+		{
+			std::stringstream sstr(valueStr);
+			int val;
+			sstr >> val;
+			return val;
+		}
+	}
+	double Value::AsDouble() const
+	{
+		if (IsNull())
+			return 0.0;
+		else
+		{
+			std::stringstream sstr(valueStr);
+			double val;
+			sstr >> val;
+			return val;
+		}
+	}
+	bool Value::AsBool() const
+	{
+		if (IsNull())
+			return false;
+		else
+			return (valueStr == "true");
+	}
+
+	void Value::SetNull()
+	{
+		valueStr = "";
+		type = VT_NULL;
+	}
+	void Value::Set(const Value &value)
+	{
+		if (this != &value)
+		{
+			valueStr = value.valueStr;
+			type = value.type;
+		}
+	}
+	void Value::Set(const std::string &value)
+	{
+		valueStr = value;
+		type = VT_STRING;
+	}
+	void Value::Set(const char *value)
+	{
+		valueStr = std::string(value);
+		type = VT_STRING;
+	}
+	void Value::Set(const int value)
+	{
+		std::stringstream sstr;
+		sstr << value;
+		valueStr = sstr.str();
+		type = VT_INT;
+	}
+	void Value::Set(const double value)
+	{
+		std::stringstream sstr;
+		sstr << value;
+		valueStr = sstr.str();
+		type = VT_DOUBLE;
+	}
+	void Value::Set(const bool value)
+	{
+		if (value)
+			valueStr = "true";
+		else
+			valueStr = "false";
+		type = VT_BOOL;
+	}
+
+	Value &Value::operator=(const Value &rhs)
+	{
+		if (this != &rhs)
+			Set(rhs);
+		return *this;
+	}
+	Value &Value::operator=(const std::string &rhs)
+	{
+		Set(rhs);
+		return *this;
+	}
+	Value &Value::operator=(const char *rhs)
+	{
+		Set(rhs);
+		return *this;
+	}
+	Value &Value::operator=(const int rhs)
+	{
+		Set(rhs);
+		return *this;
+	}
+	Value &Value::operator=(const double rhs)
+	{
+		Set(rhs);
+		return *this;
+	}
+	Value &Value::operator=(const bool rhs)
+	{
+		Set(rhs);
+		return *this;
+	}
+
+	bool Value::operator==(const Value &other) const
+	{
+		return ((type == other.type)&&(valueStr == other.valueStr));
+	}
+	bool Value::operator!=(const Value &other) const
+	{
+		return !(*this == other);
+	}
+
+	std::string Value::Write(const Format &format, unsigned int level) const
+	{
+		std::string value;
+		if (type == VT_NULL)
+			value += "null";
+		else if (type == VT_STRING)
+			value += "\""+valueStr+"\"";
+		else
+			value += valueStr;
+		return value;
+	}
+	NodePtr Value::Read(const std::string &json)
+	{
+		ValuePtr value(new Value);
+
+		if (json.front() == '"' && json.back() == '"')
+		{
+			value->valueStr = json.substr(1, json.size()-2);
+			value->type = VT_STRING;
+		}
+		else if (json == "true" || json == "false")
+		{
+			value->valueStr = json;
+			value->type = VT_BOOL;
+		}
+		else
+		{
+			bool onlyNumbers = true;
+			bool point = false;
+
+			for (std::string::const_iterator it = json.cbegin(); it != json.cend(); ++it)
+			{
+				char c = (*it);
+
+				if (c != '0' && c != '1' && c != '2' && c != '3' && c != '4' && c != '5' && c != '6' && c != '7' && c != '8' && c != '9' && c != '.')
+				{
+					onlyNumbers = false;
+				}
+				else if (c == '.')
+				{
+					point = true;
+				}
+			}
+
+			if (onlyNumbers)
+			{
+				if (point)
+				{
+					value->valueStr = json;
+					value->type = VT_DOUBLE;
+				}
+				else
+				{
+					value->valueStr = json;
+					value->type = VT_INT;
+				}
+			}
+			else
+			{
+				value->valueStr = "";
+				value->type = VT_NULL;
+			}
+		}
+
+		return value;
+	}
+
+	NodePtr Value::GetCopy() const
+	{
+		return NodePtr(new Value(*this));
+	}
+
+
+	Object::Object()
+	{
+	}
+	Object::Object(const Object &other)
+	{
+		for (ChildList::const_iterator it = other.children.cbegin(); it != other.children.cend(); ++it)
+		{
+			const std::string &name = (*it).first;
+			Node &value = *(*it).second;
+
+			children.push_back(std::make_pair<std::string, NodePtr>(name, value.GetCopy()));
+		}
+	}
+	Object::~Object()
+	{
+	}
+
+	Node::Type Object::GetType() const
+	{
+		return T_OBJECT;
+	}
+
+	void Object::Add(const std::string &name, Node &node)
+	{
+		children.push_back(std::make_pair<std::string, NodePtr>(name, node.GetCopy()));
+	}
+	void Object::Add(const std::string &name, NodePtr node)
+	{
+		children.push_back(std::make_pair<std::string, NodePtr>(name, node->GetCopy()));
+	}
+	void Object::Add(const std::string &name, Value node)
+	{
+		children.push_back(std::make_pair<std::string, NodePtr>(name, node.GetCopy()));
+	}
+	void Object::Remove(const std::string &name)
+	{
+		for (ChildList::iterator it = children.begin(); it != children.end(); ++it)
+		{
+			if ((*it).first == name)
+			{
+				children.erase(it);
+				break;
+			}
+		}
+	}
+
+	Object::Iterator Object::Begin()
+	{
+		if (children.size() > 0)
+			return Object::Iterator(&children.front());
+		else
+			return Object::Iterator(NULL);
+	}
+	Object::Iterator Object::End()
+	{
+		if (children.size() > 0)
+			return Object::Iterator(&children.back()+1);
+		else
+			return Object::Iterator(NULL);
+	}
+
+	Node &Object::Get(const std::string &name, Node &default) const
+	{
+		for (ChildList::const_iterator it = children.cbegin(); it != children.cend(); ++it)
+		{
+			if ((*it).first == name)
+			{
+				return *(*it).second;
+			}
+		}
+		return default;
+	}
+
+	std::string Object::Write(const Format &format, unsigned int level) const
+	{
+		FormatInterpreter fi(format);
+
+		std::string json;
+		json += "{" + fi.GetNewline();
+
+		for (ChildList::const_iterator it = children.cbegin(); it != children.cend(); ++it)
+		{
+			const std::string &name = (*it).first;
+			Node &value = *(*it).second;
+
+			if (it != children.cbegin())
+				json += "," + fi.GetNewline();
+			json += fi.GetIndentation(level+1) + "\""+name+"\"" + ":" + fi.GetSpacing() + value.Write(format, level+1);
+		}
+
+		json += fi.GetNewline() + fi.GetIndentation(level) + "}";
+		return json;
+	}
+	NodePtr Object::Read(const std::string &json)
+	{
+		ObjectPtr object(new Object);
+		std::string name;
+		std::string value;
+		bool atName = true;
+		int numOpen = 0;
+		bool inString = false;
+
+		for (std::string::const_iterator it = json.cbegin(); it != json.cend(); ++it)
+		{
+			char c = (*it);
+
+			if (c == '{' || c == '[')
+			{
+				++numOpen;
+			}
+			else if (c == '}' || c == ']')
+			{
+				--numOpen;
+			}
+			else if (c == '"')
+			{
+				inString = !inString;
+			}
+
+			if (atName)
+			{
+				if (!inString && c == ':')
+					atName = false;
+				else if (c != '"' && c != '{' && c != '}')
+					name += c;
+			}
+			else
+			{
+				if (((numOpen == 1 && !inString && c == ','))||(numOpen == 0))
+				{
+					if (!value.empty())
+					{
+						NodePtr node = Node::Read(value);
+						object->Add(name, *node);
+					}
+
+					name.clear();
+					value.clear();
+					atName = true;
+				}
+				else
+					value += c;
+			}
+		}
+
+		return object;
+	}
+
+	NodePtr Object::GetCopy() const
+	{
+		return NodePtr(new Object(*this));
+	}
+
+
+	Array::Array()
+	{
+	}
+	Array::Array(const Array &other)
+	{
+		for (ChildList::const_iterator it = other.children.cbegin(); it != other.children.cend(); ++it)
+		{
+			const Node &value = *(*it);
+
+			children.push_back(value.GetCopy());
+		}
+	}
+	Array::~Array()
+	{
+	}
+
+	Node::Type Array::GetType() const
+	{
+		return T_ARRAY;
+	}
+
+	void Array::Add(Node &node)
+	{
+		children.push_back(node.GetCopy());
+	}
+	void Array::Add(NodePtr node)
+	{
+		children.push_back(node->GetCopy());
+	}
+	void Array::Add(Value node)
+	{
+		children.push_back(node.GetCopy());
+	}
+	void Array::Remove(unsigned int index)
+	{
+		if (index < children.size())
+			children.erase(children.begin()+index);
+	}
+
+	Array::Iterator Array::Begin()
+	{
+		if (children.size() > 0)
+			return Array::Iterator(&children.front());
+		else
+			return Array::Iterator(NULL);
+	}
+	Array::Iterator Array::End()
+	{
+		if (children.size() > 0)
+			return Array::Iterator(&children.back()+1);
+		else
+			return Array::Iterator(NULL);
+	}
+
+	unsigned int Array::GetCount() const
+	{
+		return children.size();
+	}
+	Node &Array::Get(unsigned int index, Node &default) const
+	{
+		if (index < children.size())
+			return *children.at(index);
+		else
+			return default;
+	}
+
+	std::string Array::Write(const Format &format, unsigned int level) const
+	{
+		FormatInterpreter fi(format);
+
+		std::string json;
+		json += "[" + fi.GetNewline();
+
+		for (ChildList::const_iterator it = children.cbegin(); it != children.cend(); ++it)
+		{
+			Node &value = *(*it);
+
+			if (it != children.cbegin())
+				json += "," + fi.GetNewline();
+			json += fi.GetIndentation(level+1) + value.Write(format, level+1);
+		}
+
+		json += fi.GetNewline() + fi.GetIndentation(level) + "]";
+		return json;
+	}
+	NodePtr Array::Read(const std::string &json)
+	{
+		ArrayPtr array(new Array);
+		std::string value;
+		int numOpen = 0;
+		bool inString = false;
+
+		for (std::string::const_iterator it = json.cbegin(); it != json.cend(); ++it)
+		{
+			char c = (*it);
+
+			if (c == '{' || c == '[')
+			{
+				++numOpen;
+			}
+			else if (c == '}' || c == ']')
+			{
+				--numOpen;
+			}
+			else if (c == '"')
+			{
+				inString = !inString;
+			}
+
+			if (((numOpen == 1 && !inString && c == ','))||(numOpen == 0))
+			{
+				if (!value.empty())
+				{
+					NodePtr node = Node::Read(value);
+					array->Add(*node);
+				}
+
+				value.clear();
+			}
+			else
+			{
+				if (numOpen > 1 || (c != '[' && c != ']'))
+				value += c;
+			}
+		}
+
+		return array;
+	}
+
+	NodePtr Array::GetCopy() const
+	{
+		return NodePtr(new Array(*this));
+	}
+
+
+	FileWriter::FileWriter()
+	{
+	}
+	FileWriter::~FileWriter()
+	{
+	}
+
+	void FileWriter::WriteFile(const std::string &filename, Node &root, const Format &format)
+	{
+		FileWriter writer;
+		writer.Write(filename, root, format);
+	}
+	void FileWriter::WriteFile(const std::string &filename, NodePtr root, const Format &format)
+	{
+		WriteFile(filename, *root, format);
+	}
+
+	void FileWriter::Write(const std::string &filename, Node &root, const Format &format)
+	{
+		std::fstream file(filename.c_str(), std::ios::out | std::ios::trunc);
+		file << root.Write(format);
+		file.close();
+	}
+	void FileWriter::Write(const std::string &filename, NodePtr root, const Format &format)
+	{
+		Write(filename, *root, format);
+	}
+
+
+	FileReader::FileReader()
+	{
+	}
+	FileReader::~FileReader()
+	{
+	}
+
+	NodePtr FileReader::ReadFile(const std::string &filename)
+	{
+		FileReader reader;
+		return reader.Read(filename);
+	}
+
+	NodePtr FileReader::Read(const std::string &filename)
+	{
+		std::string json;
+		std::fstream file(filename.c_str(), std::ios::in);
+		if (!file.is_open())
+			return NodePtr(new Value());
+
+		while (!file.eof())
+		{
+			std::string line;
+			std::getline(file, line);
+			json += line + '\n';
+		}
+
+		RemoveWhitespace(json);
+
+		return Node::Read(json);
+	}
+
+	void FileReader::RemoveWhitespace(std::string &json)
+	{
+		std::string freshJson;
+
+		bool comment = false;
+		int multicomment = 0;
+		bool inString = false;
+
+		for (std::string::iterator it = json.begin(); it != json.end(); ++it)
+		{
+			char c1 = (*it);
+			char c2;
+			if (it+1 != json.end())
+				c2 = (*(it+1));
+
+			if (c1 == '"')
+			{
+				inString = !inString;
+			}
+
+			if (!inString)
+			{
+				if (c1 == '/' && c2 == '*')
+				{
+					++multicomment;
+					++it;
+					continue;
+				}
+				else if (c1 == '*' && c2 == '/')
+				{
+					--multicomment;
+					++it;
+					continue;
+				}
+				else if (c1 == '/' && c2 == '/')
+				{
+					comment = true;
+					++it;
+					continue;
+				}
+				else if (c1 == '\n')
+				{
+					comment = false;
+				}
+			}
+
+			if (comment || multicomment > 0)
+				continue;
+
+			if (inString)
+			{
+				if (c1 != '\n')
+					freshJson += c1;
+			}
+			else
+			{
+				if ((c1 != '\n')&&(c1 != ' ')&&(c1 != '\t'))
+					freshJson += c1;
+			}
+		}
+
+		json = freshJson;
+	}
+}
---
+++ tags/v1-1/Jzon.h
@@ -0,0 +1,274 @@
+/*
+Copyright (c) 2011 Johannes Häggqvist
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#ifndef Jzon_h__
+#define Jzon_h__
+
+#include <string>
+#include <vector>
+#include <utility>
+#include <memory>
+#include <iterator>
+#include <exception>
+
+namespace Jzon
+{
+	class Node;
+	typedef std::shared_ptr<Node> NodePtr;
+	typedef std::pair<std::string, NodePtr> NamedNodePtr;
+	typedef std::pair<std::string, Node&> NamedNode;
+	class Object;
+	typedef std::shared_ptr<Object> ObjectPtr;
+	class Array;
+	typedef std::shared_ptr<Array> ArrayPtr;
+	class Value;
+	typedef std::shared_ptr<Value> ValuePtr;
+	
+	class TypeException : public std::exception
+	{
+	public:
+		TypeException() : std::exception("A Node was used as the wrong type")
+		{}
+	};
+
+	struct Format
+	{
+		bool newline;
+		bool spacing;
+		bool useTabs;
+		unsigned int indentSize;
+	};
+	static const Format StandardFormat = { true, true, true, 0 };
+	static const Format NoFormat = { false, false, false, 0 };
+
+	class Node
+	{
+	public:
+		enum Type
+		{
+			T_OBJECT,
+			T_ARRAY,
+			T_VALUE
+		};
+
+		Node();
+		virtual ~Node();
+
+		virtual Type GetType() const = 0;
+
+		Object &AsObject();
+		Array &AsArray();
+		Value &AsValue();
+
+		inline bool IsObject() const { return (GetType() == T_OBJECT); }
+		inline bool IsArray() const { return (GetType() == T_ARRAY); }
+		inline bool IsValue() const { return (GetType() == T_VALUE); }
+
+		virtual std::string Write(const Format &format = NoFormat, unsigned int level = 0) const = 0;
+		static NodePtr Read(const std::string &json);
+
+		virtual NodePtr GetCopy() const = 0;
+	};
+
+	class Value : public Node
+	{
+	public:
+		enum ValueType
+		{
+			VT_NULL,
+			VT_STRING,
+			VT_INT,
+			VT_DOUBLE,
+			VT_BOOL
+		};
+
+		Value();
+		Value(const Value &rhs);
+		Value(const std::string &value);
+		Value(const char *value);
+		Value(const int value);
+		Value(const double value);
+		Value(const bool value);
+		virtual ~Value();
+
+		virtual Type GetType() const;
+		ValueType GetValueType() const;
+
+		bool IsNull() const;
+		std::string AsString() const;
+		int AsInt() const;
+		double AsDouble() const;
+		bool AsBool() const;
+
+		void SetNull();
+		void Set(const Value &value);
+		void Set(const std::string &value);
+		void Set(const char *value);
+		void Set(const int value);
+		void Set(const double value);
+		void Set(const bool value);
+
+		Value &operator=(const Value &rhs);
+		Value &operator=(const std::string &rhs);
+		Value &operator=(const char *rhs);
+		Value &operator=(const int rhs);
+		Value &operator=(const double rhs);
+		Value &operator=(const bool rhs);
+
+		bool operator==(const Value &other) const;
+		bool operator!=(const Value &other) const;
+
+		virtual std::string Write(const Format &format = NoFormat, unsigned int level = 0) const;
+		static NodePtr Read(const std::string &json);
+
+		virtual NodePtr GetCopy() const;
+
+	private:
+		std::string valueStr;
+		ValueType type;
+	};
+
+	static const Value null;
+
+	class Object : public Node
+	{
+	public:
+		class Iterator : public std::iterator<std::input_iterator_tag, NamedNodePtr>
+		{
+		public:
+			Iterator(NamedNodePtr *o) : p(o) {}
+			Iterator(const Iterator &it) : p(it.p) {}
+
+			Iterator &operator++() { ++p; return *this; }
+			Iterator operator++(int) { Iterator tmp(*this); operator++(); return tmp; }
+
+			bool operator==(const Iterator &rhs) { return p == rhs.p; }
+			bool operator!=(const Iterator &rhs) { return p != rhs.p; }
+
+			NamedNode operator*() { return NamedNode(p->first, *p->second); }
+
+		private:
+			NamedNodePtr *p;
+		};
+
+		Object();
+		Object(const Object &other);
+		virtual ~Object();
+
+		virtual Type GetType() const;
+
+		void Add(const std::string &name, Node &node);
+		void Add(const std::string &name, NodePtr node);
+		void Add(const std::string &name, Value node);
+		void Remove(const std::string &name);
+
+		Iterator Begin();
+		Iterator End();
+
+		Node &Get(const std::string &name, Node &default = Value()) const;
+
+		virtual std::string Write(const Format &format = NoFormat, unsigned int level = 0) const;
+		static NodePtr Read(const std::string &json);
+
+		virtual NodePtr GetCopy() const;
+
+	private:
+		typedef std::vector<NamedNodePtr> ChildList;
+		ChildList children;
+	};
+
+	class Array : public Node
+	{
+	public:
+		class Iterator : public std::iterator<std::input_iterator_tag, NodePtr>
+		{
+		public:
+			Iterator(NodePtr *o) : p(o) {}
+			Iterator(const Iterator &it) : p(it.p) {}
+
+			Iterator &operator++() { ++p; return *this; }
+			Iterator operator++(int) { Iterator tmp(*this); operator++(); return tmp; }
+
+			bool operator==(const Iterator &rhs) { return p == rhs.p; }
+			bool operator!=(const Iterator &rhs) { return p != rhs.p; }
+
+			Node &operator*() { return **p; }
+
+		private:
+			NodePtr *p;
+		};
+
+		Array();
+		Array(const Array &other);
+		virtual ~Array();
+
+		virtual Type GetType() const;
+
+		void Add(Node &node);
+		void Add(NodePtr node);
+		void Add(Value node);
+		void Remove(unsigned int index);
+
+		Iterator Begin();
+		Iterator End();
+
+		unsigned int GetCount() const;
+		Node &Get(unsigned int index, Node &default = Value()) const;
+
+		virtual std::string Write(const Format &format = NoFormat, unsigned int level = 0) const;
+		static NodePtr Read(const std::string &json);
+
+		virtual NodePtr GetCopy() const;
+
+	private:
+		typedef std::vector<NodePtr> ChildList;
+		ChildList children;
+	};
+
+	class FileWriter
+	{
+	public:
+		FileWriter();
+		~FileWriter();
+
+		static void WriteFile(const std::string &filename, Node &root, const Format &format = NoFormat);
+		static void WriteFile(const std::string &filename, NodePtr root, const Format &format = NoFormat);
+
+		void Write(const std::string &filename, Node &root, const Format &format = NoFormat);
+		void Write(const std::string &filename, NodePtr root, const Format &format = NoFormat);
+	};
+
+	class FileReader
+	{
+	public:
+		FileReader();
+		~FileReader();
+
+		static NodePtr ReadFile(const std::string &filename);
+
+		NodePtr Read(const std::string &filename);
+
+	private:
+		void RemoveWhitespace(std::string &json);
+	};
+}
+
+#endif // Jzon_h__
